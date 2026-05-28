86575017e945e5f38b64ff6a72da0f56735185fc
Vitor Martins <martinsvitor@gmx.de>
2026-05-28 19:31:50 +0200

    fixing bugs for deployment

--- app/api/tweets/route.ts
+++ app/api/tweets/route.ts
@@ -1,38 +1,7 @@
-import fileSystem from 'node:fs/promises';
 import {NextResponse} from 'next/server';
-import path from 'path';
-import {Database, TweetWithAuthor} from '@/app/lib/definitions';
+import {getTweetsWithAuthor} from '@/app/lib/data';
 
 export async function GET() {
-    const filePath = path.join(process.cwd(), 'db.json');
-    const rawData = await fileSystem.readFile(filePath, {encoding: 'utf8'});
-    const database: Database = JSON.parse(rawData);
-
-    // Join user data with tweets and calculate reaction counts
-    const tweetsWithAuthor: TweetWithAuthor[] = database.tweets.map(tweet => {
-        const author = database.users.find(user => user.id === tweet.authorId);
-        if (!author) {
-            throw new Error(`Author not found for tweet ${tweet.id}`);
-        }
-
-        const likesCount = database.likes.filter(like => like.tweetId === tweet.id).length;
-        const dislikesCount = database.dislikes.filter(dislike => dislike.tweetId === tweet.id).length;
-
-        return {
-            id: tweet.id,
-            username: author.username,
-            handle: author.handle,
-            avatar: author.avatar,
-            content: tweet.content,
-            timestamp: tweet.createdAt,
-            reactions: {
-                likes: likesCount,
-                dislikes: dislikesCount
-            },
-            views: tweet.views,
-            userid: tweet.authorId
-        };
-    });
-
+    const tweetsWithAuthor = await getTweetsWithAuthor();
     return NextResponse.json(tweetsWithAuthor);
 }
