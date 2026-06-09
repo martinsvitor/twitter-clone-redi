e713025b0e5f7cd716edba437fed3b7b6649fb41
Vitor Martins <martinsvitor@gmx.de>
2026-06-09 16:53:33 +0200

    Add user profile pages with protected routes; add user search functionality; implement custom error handling; enhance NextAuth with JWT user ID; refactor Navbar component; update Prisma schema

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,11 @@
 import {auth} from '@/auth';
 import {NextResponse} from 'next/server';
 
-export default auth((req => {
+export default auth((req) => {
     if (!req.auth) {
         return NextResponse.redirect(new URL("/auth", req.url));
     }
-}))
+});
 
 export const config = {
     matcher: [
